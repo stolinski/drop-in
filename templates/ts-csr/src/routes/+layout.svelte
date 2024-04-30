1b6d1403af575531edcbaba413c3c06a4c3c8d97
Scott Tolinski <scott.tolinski@gmail.com>
2024-04-30 15:30:29 -0600

    starts to work on menu component

--- templates/ts-csr/src/routes/+layout.svelte
+++ templates/ts-csr/src/routes/+layout.svelte
@@ -3,7 +3,31 @@
 	import { Toast, toast } from '@slick-kit/toast';
 	let { children } = $props()
 
+const modules = import.meta.glob('$routes/**', { eager: true });
 
+const folderNames = Object.keys(modules).map((path) => {
+  const segments = path.split('/');
+  return segments.slice(0, -1).join('/');
+});
+
+const uniqueFolderNames = [...new Set(folderNames)];
+
+const transformedArray = uniqueFolderNames
+  .map((folderName) => {
+    const segments = folderName.split("/");
+    const name = segments[segments.length - 1]
+      .replace(/[-_]/g, " ")
+      .replace(/\[.*?\]/g, "")
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+		const link = "/" + segments.slice(2).join("/");
+    return { name, link };
+  })
+  .filter((item) => item.name !== "(app)" && item.name !== "(site)")
+	.filter((item) => !item.link.includes("["));
+
+console.log(transformedArray);
 </script>
 
 <!-- If you would like to use the same layout in app and site, use this file -->
