354abccba37d26ea52022896faaac007f1f53cc6
Scott Tolinski <scott.tolinski@gmail.com>
2024-04-30 07:45:13 -0600

    changes a slot to children and moves toast to outer layout

--- templates/ts-csr/src/routes/+layout.svelte
+++ templates/ts-csr/src/routes/+layout.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	import { Toast } from '@slick-kit/toast';
+	let { children } = $props()
+</script>
+
 <!-- If you would like to use the same layout in app and site, use this file -->
-<slot />
+{@render children()}
+
+<!--  
+	SickToast -> just use any of these methods
+<button onclick={() => toast.send('Test Toast')}>Test Toast</button>
+<button onclick={() => toast.warning('Test Toast')}>Test Toast</button>
+<button onclick={() => toast.error('Test Toast')}>Test Toast</button>
+<button onclick={() => toast.success('Test Toast')}>Test Toast</button>
+<button onclick={() => toast.info('Test Toast')}>Test Toast</button>
+-->
+<Toast position={{ inline: 'end', block: 'end' }} offset={{ inline: '20px', block: '20px' }} />
