94bb082ce3b14e09554769cc22334777050357ce
Scott Tolinski <scott.tolinski@gmail.com>
2024-09-18 22:04:12 -0600

    adds drawer and dialog to index

--- packages/decks/src/lib/index.ts
+++ packages/decks/src/lib/index.ts
@@ -1,4 +1,6 @@
 // Reexport your entry components here
+export { default as Dialog } from './Dialog.svelte';
+export { default as Drawer } from './Drawer.svelte';
 export { default as Menu } from './Menu.svelte';
 export { default as Pill } from './Pill.svelte';
 export { default as Pills } from './Pills.svelte';
